cb8c7cc379d9df5c041a87136f8ce891f091f3b0
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-05-27 10:28:40 -0500

    :hammer: refactor date formatDateCol

--- .github/actions/semantic-release/.releaserc.js
+++ .github/actions/semantic-release/.releaserc.js
@@ -40,6 +40,7 @@ module.exports = {
               return string.trim().split('\n');
             },
             formatDateCol: function(date) {
+              if(!date) date = new Date();
               return date.toLocaleString('en-US', { timeZone: 'America/Bogota', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }).replace(',','');
             }
           }
